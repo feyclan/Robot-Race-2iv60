fc5eff6c1ac17f1685efe14d4bcba4b97d540d9a
nours <nours11@msn.com>
2019-10-17 01:03:59 +0200

    hierarchy
    armen en benen hebben geen joints; geen prioriteit

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -41,42 +41,40 @@ class Robot {
         gl.glPushMatrix();        
         gl.glTranslated(0,0,4.5);
         glut.glutSolidCube(1); //head
+        drawTorso(gl, glu, glut);
+        drawRightArm(gl, glu, glut);
+        drawLeftArm(gl, glu, glut);
+        drawLeftLeg(gl, glu, glut);
+        drawRightLeg(gl, glu, glut);
+        gl.glPopMatrix();
+    }
+    
+    public void drawTorso(GL2 gl, GLU glu, GLUT glut){
         gl.glColor3d(0,0,0);
         gl.glTranslated(0,0,-1.5);
         gl.glScaled(1,1,2);
         glut.glutSolidCube(1); //torso
+    }
+    
+    public void drawRightArm(GL2 gl, GLU glu, GLUT glut){
         gl.glColor3d(0,0.3,0);
         gl.glTranslated(0.75, 0, 0);
         gl.glScaled(0.5,1,1);
         glut.glutSolidCube(1); //first arm
-        gl.glTranslated(-3, 0, 0);
-        glut.glutSolidCube(1); // second arm
-        gl.glTranslated(1, 0, -1);
-        glut.glutSolidCube(1); //first leg
-        gl.glTranslated(1,0,0);
-        glut.glutSolidCube(1); //second leg
-        gl.glPopMatrix();
     }
     
-    public void niks(GL2 gl, GLU glu, GLUT glut){
-        gl.glColor3d(0.5,0.5,0.5);
-        gl.glTranslated(0,0,2.5);
-        glut.glutSolidCube(1); // head
-        gl.glColor3d(0.1,0.1,0.1);
-        gl.glTranslated(0,0,-1.5);
-        gl.glScaled(1.5,1,2);
-        glut.glutSolidCube(1); //torso
-        gl.glColor3d(0.6, 0.4,0.5);
-        gl.glScaled(0.5/1.5, 1, 0.9);
-        gl.glTranslated(0.75+1.25,0,0);
-        glut.glutSolidCube(1);
-        gl.glTranslated(-4,0,0);
-        glut.glutSolidCube(1);//arms
-        gl.glTranslated(2.75,0,-1.025);
-        gl.glScaled(1.3,1,1);
-        glut.glutSolidCube(1);
-        gl.glTranslated(-1.25, 0,0);
-        glut.glutSolidCube(1);//legs
-        //WAARDES LATEN KLOPPEN
+    public void drawLeftArm(GL2 gl, GLU glu, GLUT glut){
+        gl.glTranslated(-3, 0, 0);
+        glut.glutSolidCube(1); // second arm
+    }
+    
+    public void drawLeftLeg(GL2 gl, GLU glu, GLUT glut){
+        gl.glTranslated(1, 0, -1);
+        glut.glutSolidCube(1); //first leg
+    }
+    
+    public void drawRightLeg(GL2 gl, GLU glu, GLUT glut){
+        gl.glTranslated(1,0,0);
+        glut.glutSolidCube(1); //second leg
     }
 }
