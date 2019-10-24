627fd3aa108685e67a9d7f072fd09924a015f83a
nours <nours11@msn.com>
2019-10-24 18:36:21 +0200

    heihgt map

--- build/classes/robotrace/shaderPrograms/Terrain/vertex.glsl
+++ build/classes/robotrace/shaderPrograms/Terrain/vertex.glsl
@@ -2,6 +2,8 @@
 
 void main()
 {
-    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
+    vec4 height = gl_Vertex;
+    height.z = (0.6 * cos(0.3 * height.x + 0.2 * height.y)) + (0.4 * cos(height.x - 0.5 * height.y));
+    gl_Position    = gl_ModelViewProjectionMatrix * height;      // model view transform
     gl_FrontColor = gl_Color;
 }
